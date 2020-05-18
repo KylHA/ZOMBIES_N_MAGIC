730ee04cf56800974cbff5a170f14547dbae0610
KylHA <programlamakitabi@gmail.com>
2020-05-19 01:43:19 +0300

    changedLookAt

--- Assets/Test/Scripts/Player/PlayerLookMouse.cs
+++ Assets/Test/Scripts/Player/PlayerLookMouse.cs
@@ -9,29 +9,35 @@ public class PlayerLookMouse : MonoBehaviour
     GameObject bullet, ball;
     RaycastHit hit;
     float speed = 8f;
+    GameObject lookObj;
+
 
     int layerMask = 1 << 8;
 
-    // This would cast rays only against colliders in layer 8.
-    // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+    
     private void Start()
     {
-        layerMask = ~layerMask;
+        layerMask = ~layerMask;//invert layer mask
+        lookObj = new GameObject();
     }
     void Update()
     {
+        RayLookat();
+
+        LooktoObj();
+    }
+
+    void RayLookat() 
+    {
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit))
         {
-            transform.LookAt(new Vector3(hit.point.x,transform.position.y, hit.point.z));
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
         }
 
-        if(Input.GetMouseButtonDown(0))
-        {
-            bullet = Instantiate(bulletPrefab, GameObject.Find("Barrel").GetComponent<Transform>().position, GameObject.Find("Barrel").GetComponent<Transform>().rotation) as GameObject;
-            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 5100);
-        }
+        
         if (Physics.Raycast(ray, out hit) && Input.GetKeyDown(KeyCode.V))
         {
             ball = Instantiate(BallPrefab, GameObject.Find("Barrel").GetComponent<Transform>().position, GameObject.Find("Barrel").GetComponent<Transform>().rotation) as GameObject;
@@ -42,13 +48,21 @@ public class PlayerLookMouse : MonoBehaviour
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.black);
             hit.collider.transform.parent.SendMessage("ApplyDamage", 5);
-           
+
         }
 
-        
-        else
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 40f, Color.white);
+            bullet = Instantiate(bulletPrefab, GameObject.Find("Barrel").GetComponent<Transform>().position, GameObject.Find("Barrel").GetComponent<Transform>().rotation) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 5100);
         }
+    }
+
+    void LooktoObj() 
+    {
+        lookObj.transform.position =new Vector3(Input.mousePosition.x,.5f,Input.mousePosition.z);
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * Vector3.Distance(transform.position,lookObj.transform.position), Color.white);
+
     }
 }
